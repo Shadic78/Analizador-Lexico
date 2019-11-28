72ea9e938a8e061ca9f5de319700db74bde451eb
Shadic78 <carlosjosue7889@gmail.com>
2019-11-28 16:28:02 -0600

    Agregado seleccionar ruta de destino

---
+++ src/codigo/CrearLexer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ *  IGNORAR, SOLO EJECUTAR SI SE MODIFICA Lexer.flex
+ */
+public class CrearLexer {
+    /*public static void main(String[] args) {
+        String ruta = "C:/Users/Usuario.ATHENEA/Desktop/AnalizadorLexico/src/codigo/Lexer.flex";
+        generarLexer(ruta);
+    }
+    public static void generarLexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }*/
+}
